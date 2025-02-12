a8ac8c2ffbe71978b1acf54d253dfa371d41733c
이정원 <jwon133@gmail.com>
2025-02-12 21:18:31 +0900

    간단한 별자리 북두칠성 구현

---
+++ my-constellation/src/App.jsx
@@ -0,0 +1,12 @@
+import { Scene } from "./components/canvas/Scene";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Scene />
+    </div>
+  );
+}
+
+export default App;
